7157c6aeaaded5cd11af7ddf8a8ed50c8a9abf71
Rodrigo Passos <rodrigoarapassos@gmail.com>
2023-03-15 15:48:20 -0300

    Add basic option PvP/PvC

--- src/index.js
+++ src/index.js
@@ -12,20 +12,13 @@ const start = (gameType) => {
 
 const gameOpt = () => {
     const playVs = document.querySelector(".playVs");
-    //playVs.style.visibility == "hidden" ? playVs.style.visibility = "visible" : playVs.style.visibility = "hidden";
-    if (playVs.style.visibility == "hidden") {
-        playVs.style.visibility = "visible";
-        //document.querySelector("button.PvC").addEventListener("click", ()=> start);
-        //document.querySelector(".PvP").addEventListener("click", start("pvp"));
-    }else {
-        playVs.style.visibility = "hidden";
-        //document.querySelector("button.PvC").removeEventListener("click", ()=> start);
-        //document.querySelector(".PvP").removeEventListener("click", start("pvp"));
-    }
+    playVs.style.visibility == "hidden" ? playVs.style.visibility = "visible" : playVs.style.visibility = "hidden";
 }
 
 const newGame = (() => {
     const playVs = document.querySelector(".playVs");
     playVs.style.visibility = "hidden";
     document.querySelector(".newGameBtn").addEventListener("click", gameOpt);
+    document.querySelector("button.PvC").addEventListener("click", ()=> start("pvc"));
+    document.querySelector(".PvP").addEventListener("click",()=> start("pvp"));
 })();
