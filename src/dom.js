24151d112c45101dd127be665b49166570bb48bb
Rodrigo Passos <rodrigoarapassos@gmail.com>
2023-03-21 17:14:56 -0300

    Add VxH option for player ships placement

--- src/dom.js
+++ src/dom.js
@@ -6,13 +6,10 @@ function dragData (ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
-/* function dropToPlace (ev) {
-    let shipType = ev.dataTransfer.getData("text");
-    let index = ev.target.getAttribute("data-index");
-    p1.Gameboard.place(index, "h", shipType);
-    ev.preventDefault();
-    displayBoards(p1, p2, "pvcPlace");
-} */
+function orientChange (ev) {
+    ev.target.getAttribute("data-orientation") == "h" ? 
+        ev.target.setAttribute("data-orientation", "v") : ev.target.setAttribute("data-orientation", "h");
+}
 
 let listOfShips = [];
 const allShipsList = ["patrol", "submarine", "destroyer", "battleship", "carrier"];
@@ -23,10 +20,6 @@ const displayBoards = (p1, p2, gameType) => {
     const messageContainer = document.querySelector(".winner");
     const loading = document.querySelector(".waiting");
 
-    /* while (shipBoards1.firstChild && attackBoard1.firstChild){
-        shipBoards1.removeChild(shipBoards1.firstChild);
-        attackBoard1.removeChild(attackBoard1.firstChild);
-    } */
 
     switch (gameType) {
         case "pvcPlace": //place ships vs computer
@@ -40,8 +33,7 @@ const displayBoards = (p1, p2, gameType) => {
             const cell = document.createElement("div");
             cell.classList.add("cell");
             cell.setAttribute("data-index", p1.Gameboard.grid.indexOf(element));
-            //cell.setAttribute("ondrop", "dropToPlace(event)");
-            //cell.setAttribute("ondragover", "allowDrop(event)");
+
             if (element.hasShip == true) cell.classList.add("hasShip");
             if (element.wasShot == true && element.hasShip == false) cell.classList.add("waterHit");
             if (element.wasShot == true && element.hasShip == true) cell.classList.add("shipHit");
@@ -50,8 +42,9 @@ const displayBoards = (p1, p2, gameType) => {
                 cell.addEventListener("drop", function dropToPlace (ev) {
                     ev.preventDefault();
                     let shipType = ev.dataTransfer.getData("text");
+                    let orientation = document.getElementById(shipType).getAttribute("data-orientation");
                     let index = ev.target.getAttribute("data-index");
-                    let shipPlaced = p1.Gameboard.place(Number(index), "v", shipType);
+                    let shipPlaced = p1.Gameboard.place(Number(index), orientation, shipType);
                     listOfShips.push(shipPlaced);
                     displayBoards(p1, p2, "pvcPlace");
                 });
@@ -139,51 +132,6 @@ const displayBoards = (p1, p2, gameType) => {
 
         break;
     }
-
-    /* //clear for display updated
-    const shipBoards1 = document.querySelector(".player1 .shipsBoard");
-    const attackBoard1 = document.querySelector(".player1 .attackBoard");
-    const messageContainer = document.querySelector(".winner");
-
-    messageContainer.innerHTML = "Player 1";
-    shipBoards1.style.visibility = "visible";
-    attackBoard1.style.visibility = "visible";
-
-    while (shipBoards1.firstChild && attackBoard1.firstChild){
-        shipBoards1.removeChild(shipBoards1.firstChild);
-        attackBoard1.removeChild(attackBoard1.firstChild);
-    }
-
-    //board of ships
-    for (let element of p1.Gameboard.grid) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.setAttribute("data-index", p1.Gameboard.grid.indexOf(element));
-        if (element.hasShip == true) cell.classList.add("hasShip");
-        if (element.wasShot == true && element.hasShip == false) cell.classList.add("waterHit");
-        if (element.wasShot == true && element.hasShip == true) cell.classList.add("shipHit");
-        if (element.shipName != null && element.shipName.isSunk() == true) cell.classList.add("shipSunk");
-        shipBoards1.appendChild(cell);
-    }
-
-    //board of attacks
-    for (let element of p2.Gameboard.grid) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.setAttribute("data-index", p2.Gameboard.grid.indexOf(element));
-        if (element.wasShot == true && element.hasShip == true) cell.classList.add("shipHit");
-        if (element.wasShot == true && element.hasShip == false) cell.classList.add("waterHit");
-        if (element.shipName != null && element.shipName.isSunk() == true) cell.classList.add("shipSunk");
-        if (element.wasShot == false) {
-            cell.addEventListener("click", ()=> {
-                p1.attackPlayer(cell.getAttribute("data-index"), p2);
-                p2.computerAttack(p1);
-                displayBoards(p1, p2);
-                checkWinner(p1, p2);
-            });
-        }
-        attackBoard1.appendChild(cell);
-    } */
 }
 
 const displayShipList = () => {
@@ -202,37 +150,43 @@ const displayShipList = () => {
     patrolLi.innerHTML = "Patrol";
     if (listOfShips.includes("patrol")) patrolLi.style.visibility = "hidden";
     patrolLi.setAttribute("id", "patrol");
+    patrolLi.setAttribute("data-orientation", "h");
     patrolLi.setAttribute("draggable", "true");
+    patrolLi.addEventListener("click", orientChange);
     patrolLi.addEventListener("dragstart", dragData);
-    //patrolLi.setAttribute("ondragstart", "dragData(event)");
     const submarineLi = document.createElement("li");
     submarineLi.innerHTML = "Submarine";
     if (listOfShips.includes("submarine")) submarineLi.style.visibility = "hidden";
     submarineLi.setAttribute("id", "submarine");
+    submarineLi.setAttribute("data-orientation", "h");
     submarineLi.setAttribute("draggable", "true");
+    submarineLi.addEventListener("click", orientChange);
     submarineLi.addEventListener("dragstart", dragData);
-    //submarineLi.setAttribute("ondragstart", "dragData(event)");
     const destroyerLi = document.createElement("li");
     destroyerLi.innerHTML = "Destroyer";
     if (listOfShips.includes("destroyer")) destroyerLi.style.visibility = "hidden";
     destroyerLi.setAttribute("id", "destroyer");
+    destroyerLi.setAttribute("data-orientation", "h");
     destroyerLi.setAttribute("draggable", "true");
+    destroyerLi.addEventListener("click", orientChange);
     destroyerLi.addEventListener("dragstart", dragData);
-    //destroyerLi.setAttribute("ondragstart", "dragData(event)");
     const battleshipLi = document.createElement("li");
     battleshipLi.innerHTML = "Battleship";
     if (listOfShips.includes("battleship")) battleshipLi.style.visibility = "hidden";
     battleshipLi.setAttribute("id", "battleship");
+    battleshipLi.setAttribute("data-orientation", "h");
     battleshipLi.setAttribute("draggable", "true");
+    battleshipLi.addEventListener("click", orientChange);
     battleshipLi.addEventListener("dragstart", dragData);
-    //battleshipLi.setAttribute("ondragstart", "dragData(event)");
     const carrierLi = document.createElement("li");
     carrierLi.innerHTML = "Carrier";
     if (listOfShips.includes("carrier")) carrierLi.style.visibility = "hidden";
     carrierLi.setAttribute("id", "carrier");
+    carrierLi.setAttribute("data-orientation", "h");
     carrierLi.setAttribute("draggable", "true");
+    carrierLi.addEventListener("click", orientChange);
     carrierLi.addEventListener("dragstart", dragData);
-    //carrierLi.setAttribute("ondragstart", "dragData(event)");
+    //append
     shipList.appendChild(patrolLi);
     shipList.appendChild(submarineLi);
     shipList.appendChild(destroyerLi);
