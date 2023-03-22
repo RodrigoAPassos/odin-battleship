463253b1b27e61858d7722c707395f831b590cc1
Rodrigo Passos <rodrigoarapassos@gmail.com>
2023-03-22 14:40:54 -0300

    Add check for ship overlap

--- src/gameboard.js
+++ src/gameboard.js
@@ -9,7 +9,18 @@ const Gameboard = () => {
 
     const place = (position, orientation, shipType) => {
         if (shipType == "patrol") {
+            //check for free positions
+            for (let i = 0; i < 1; i++) {
+                if (orientation == "h") {
+                    if (grid[position + i].hasShip == false) continue;
+                    else if (grid[position + i].hasShip == true) return;
+                } else {
+                    if (grid[position + (i * 10)].hasShip == false) continue;
+                    else if (grid[position + (i * 10)].hasShip == true) return;
+                }
+            }
             const patrol = ship(1);
+            //place
             for (let i = 0; i < 1; i++) {
                 if (orientation == "h") { 
                     grid[position + i].hasShip = true;
@@ -22,6 +33,16 @@ const Gameboard = () => {
         }else if (shipType == "submarine") {
             if (orientation == "h" && /[9]$/.test(position)) return;
             if (orientation == "v" && position > 89) return;
+            //check for free positions
+            for (let i = 0; i < 2; i++) {
+                if (orientation == "h") {
+                    if (grid[position + i].hasShip == false) continue;
+                    else if (grid[position + i].hasShip == true) return;
+                } else {
+                    if (grid[position + (i * 10)].hasShip == false) continue;
+                    else if (grid[position + (i * 10)].hasShip == true) return;
+                }
+            }
             const submarine = ship(2);
             for (let i = 0; i < 2; i++) {
                 if (orientation == "h") { 
@@ -35,6 +56,17 @@ const Gameboard = () => {
         }else if (shipType == "destroyer") {
             if (orientation == "h" && /[8-9]$/.test(position)) return;
             if (orientation == "v" && position > 79) return;
+            //check for free positions
+            for (let i = 0; i < 3; i++) {
+                if (orientation == "h") {
+                    if (grid[position + i].hasShip == false) continue;
+                    else if (grid[position + i].hasShip == true) return;
+                } else {
+                    if (grid[position + (i * 10)].hasShip == false) continue;
+                    else if (grid[position + (i * 10)].hasShip == true) return;
+                }
+            }
+            //place
             const destroyer = ship(3);
             for (let i = 0; i < 3; i++) {
                 if (orientation == "h") { 
@@ -48,6 +80,17 @@ const Gameboard = () => {
         }else if (shipType == "battleship") {
             if (orientation == "h" && /[7-9]$/.test(position)) return;
             if (orientation == "v" && position > 69) return;
+            //check for free positions
+            for (let i = 0; i < 4; i++) {
+                if (orientation == "h") {
+                    if (grid[position + i].hasShip == false) continue;
+                    else if (grid[position + i].hasShip == true) return;
+                } else {
+                    if (grid[position + (i * 10)].hasShip == false) continue;
+                    else if (grid[position + (i * 10)].hasShip == true) return;
+                }
+            }
+            //place
             const battleship = ship(4);
             for (let i = 0; i < 4; i++) {
                 if (orientation == "h") { 
@@ -61,6 +104,16 @@ const Gameboard = () => {
         }else if (shipType == "carrier") {
             if (orientation == "h" && /[6-9]$/.test(position)) return;
             if (orientation == "v" && position > 59) return;
+            //check for free positions
+            for (let i = 0; i < 5; i++) {
+                if (orientation == "h") {
+                    if (grid[position + i].hasShip == false) continue;
+                    else if (grid[position + i].hasShip == true) return;
+                } else {
+                    if (grid[position + (i * 10)].hasShip == false) continue;
+                    else if (grid[position + (i * 10)].hasShip == true) return;
+                }
+            }
             const carrier = ship(5);
             for (let i = 0; i < 5; i++) {
                 if (orientation == "h") { 
